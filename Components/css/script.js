47f2f941c9fdca943d5a66e8d8dffecea3c15b78
Sarah Gephart <sgephart@nic.edu>
2023-03-22 19:46:07 -0700

    Final Ver 6

--- Components/css/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('.toggle-collapse');
-let topnav = document.querySelector ('.topnav');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    topbar.classList.toggle('open');
-}
